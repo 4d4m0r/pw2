35b266053f2706baee28c5891a22467b4f09e456
4d4m0r <adamorbarbosa0@gmail.com>
2024-02-28 15:17:24 -0300

    comentarios

--- express/backend/src/resources/compra/compra.service.ts
+++ express/backend/src/resources/compra/compra.service.ts
@@ -3,6 +3,15 @@ import { Compra, CompraItem } from './compra.types';
 
 const prisma = new PrismaClient();
 
-export async function adicionarProdutoAoCarrinho(produtoId: string, quantidade: number): Promise<void> {}
+export async function adicionarProdutoAoCarrinho(produtoId: string, quantidade: number): Promise<void> {
+   // Tenho que verificar se o produto existe
+   // Se ele existe verificar se a quantidade pedida existe
+   // Se as condições acima forem ok adicionar na tabela compraItem
+}
 
-export async function concluirCompra(usuarioId: string): Promise<Compra> {}
+export async function concluirCompra(usuarioId: string): Promise<Compra> {
+    // Verificar se o usuario possui produtos na tabela compraItem
+    // Crio a compra na tabela compra
+    // Atualizo o estoque de produtos na tabela Produtos
+    // Limpo o carrinho do usuário na tabela compraItem
+}
